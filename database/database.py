c0e563c486f313288de2156bc69b8ef49d3bba96
DJsega1 <68695859+DJsega1@users.noreply.github.com>
2021-06-10 18:21:19 +0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -14,31 +14,82 @@ users = Table('users', metadata,
               Column('first_name', VARCHAR(30), nullable=False),
               Column('last_name', VARCHAR(40), nullable=False),
               Column('email', VARCHAR(50), nullable=False),
-              Column('cities_id', Integer, ForeignKey('cities.id', ondelete="CASCADE", onupdate="CASCADE"),
+              Column('city_id', Integer, ForeignKey('cities.id', ondelete="CASCADE", onupdate="CASCADE"),
                      nullable=False)
               )
 
 conn = engine.connect()
 metadata.create_all(engine)
 
+cities_data = [
+    {"title": "Саранск"},
+    {"title": "Москва"},
+    {"title": "Челябинск"},
+    {"title": "Воронеж"}
+]
 
-def execu(q):
-    conn.execute(q)
+users_data = [
+    {
+        "first_name": "Александр",
+        "last_name": "Смутин",
+        "email": "asd@mail.ru",
+        "city_id": 3
+    },
+    {
+        "first_name": "Мария",
+        "last_name": "Волкова",
+        "email": "volk@gmail.com",
+        "city_id": 2
+    },
+    {
+        "first_name": "Павел",
+        "last_name": "Гришин",
+        "email": "pudge@yandex.ru",
+        "city_id": 1
+    },
+    {
+        "first_name": "Яна",
+        "last_name": "Вовкина",
+        "email": "yana@mail.ru",
+        "city_id": 4
+    },
+    {
+        "first_name": "Елена",
+        "last_name": "Исаева",
+        "email": "isa@mail.ru",
+        "city_id": 2
+    },
+    {
+        "first_name": "Михаил",
+        "last_name": "Пивкин",
+        "email": "misha@gmail.com",
+        "city_id": 2
+    },
+    {
+        "first_name": "Георгий",
+        "last_name": "Жуков",
+        "email": "juke@mail.ru",
+        "city_id": 3
+    },
+    {
+        "first_name": "Анна",
+        "last_name": "Казакова",
+        "email": "anya@gmail.com",
+        "city_id": 1
+    },
+    {
+        "first_name": "Иван",
+        "last_name": "Морозов",
+        "email": "moroz@yandex.ru",
+        "city_id": 2
+    },
+    {
+        "first_name": "Станислав",
+        "last_name": "Кошкин",
+        "email": "stasik@mail.ru",
+        "city_id": 4
+    }
+]
 
-
-execu(insert(cities).values(title="Саранск"))
-execu(insert(cities).values(title="Москва"))
-execu(insert(cities).values(title="Челябинск"))
-execu(insert(cities).values(title="Воронеж"))
-execu(insert(users).values(first_name="Александр", last_name="Смутин", email="asd@mail.ru", cities_id=3))
-execu(insert(users).values(first_name="Мария", last_name="Волкова", email="volk@gmail.com", cities_id=2))
-execu(insert(users).values(first_name="Павел", last_name="Гришин", email="pudge@yandex.ru", cities_id=1))
-execu(insert(users).values(first_name="Яна", last_name="Вовкина", email="yana@mail.ru", cities_id=4))
-execu(insert(users).values(first_name="Елена", last_name="Исаева", email="isa@mail.ru", cities_id=2))
-execu(insert(users).values(first_name="Михаил", last_name="Пивкин", email="misha@gmail.com", cities_id=2))
-execu(insert(users).values(first_name="Георгий", last_name="Жуков", email="juke@mail.ru", cities_id=3))
-execu(insert(users).values(first_name="Анна", last_name="Казакова", email="anya@gmail.com", cities_id=1))
-execu(insert(users).values(first_name="Иван", last_name="Морозов", email="moroz@yandex.ru", cities_id=2))
-execu(insert(users).values(first_name="Станислав", last_name="Кошкин", email="stasik@mail.ru", cities_id=4))
-
-conn.close()
+conn.execute(insert(cities), cities_data)
+conn.execute(insert(users), users_data)
